515a29b221e6691df2048fc0cad20ca42021898a
nickc-ci <nchamberlain@cambridge-intl.com>
2016-06-19 14:13:29 -0400

    Bob

--- src/Exercism/Bob.fs
+++ src/Exercism/Bob.fs
@@ -7,14 +7,21 @@ module Bob =
     let (|IsMatch|_|) (re : string) (inp : string) =
         if Regex(re).IsMatch(inp)  then Some() else None
 
-    let isAllCaps (s:string) =
-        s.Split ' '
-        |> Array.exists(fun x -> Regex(x).IsMatch("[a-z]"))
-        
+    let isAllCaps s =
+        match s with
+        | s when String.exists (fun c -> System.Char.IsLower(c)) s -> false
+        | _ -> true
+      
+    let hasAnyLetters s =
+        match s with
+        | s when String.exists (fun c -> System.Char.IsLetter(c)) s -> true
+        | _ -> false
 
     let hey words = 
         match words with
-        | IsMatch "[!$]" -> "Whoa, chill out!"
-        |  -> "Whoa, chill out!"
-        | IsMatch "[?$]" -> "Sure."
+        | words when 
+            hasAnyLetters words 
+            && isAllCaps words -> "Whoa, chill out!"
+        | IsMatch "[?]$" -> "Sure."
+        | IsMatch "^\s*$" -> "Fine. Be that way!"
         | _ -> "Whatever."
