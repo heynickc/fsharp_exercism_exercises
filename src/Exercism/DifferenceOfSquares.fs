84869a25425a3c6ae0f3095147ec71014d235e24
nickc-ci <nchamberlain@cambridge-intl.com>
2016-06-19 21:36:03 -0400

    SpaceAge

---
+++ src/Exercism/DifferenceOfSquares.fs
@@ -0,0 +1,17 @@
+namespace Exercism
+
+module DifferenceOfSquares = 
+    
+    let square x = x * x
+
+    let squareOfSums n =
+        [1 .. n]
+        |> Seq.sum 
+        |> square
+
+    let sumOfSquares n = 
+        [1 .. n]
+        |> Seq.sumBy(square)
+
+    let difference n =
+        squareOfSums n - sumOfSquares n
---
+++ src/Exercism/SpaceAge.fs
@@ -0,0 +1,28 @@
+﻿namespace Exercism
+
+module SpaceAge = 
+
+    type Planet =
+        | Earth
+        | Mercury
+        | Venus
+        | Mars
+        | Jupiter
+        | Saturn
+        | Uranus
+        | Neptune
+
+    let spaceAge planet seconds = 
+        let earthSeconds = 31557600m
+        let round (n:decimal) = System.Math.Round(n, 2)
+        match planet with
+        | Earth -> round (seconds/earthSeconds)
+        | Mercury -> round (seconds/(earthSeconds*0.2408467m))
+        | Venus -> round (seconds/(earthSeconds*0.61519726m))
+        | Mars -> round (seconds/(earthSeconds*1.8808158m))
+        | Jupiter -> round (seconds/(earthSeconds*11.862615m))
+        | Saturn -> round (seconds/(earthSeconds*29.447498m))
+        | Uranus -> round (seconds/(earthSeconds*84.016846m))
+        | Neptune -> round (seconds/(earthSeconds*164.79132m))
+        | _ -> 0m
+
