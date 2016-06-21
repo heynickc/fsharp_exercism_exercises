8353995edc64555d8c228686d939fb235d08e22c
nickc-ci <nchamberlain@cambridge-intl.com>
2016-06-20 20:49:38 -0400

    triangle

--- src/Exercism/Bob.fs
+++ src/Exercism/Bob.fs
@@ -5,15 +5,17 @@ module Bob =
     open System.Text.RegularExpressions
 
     let (|IsMatch|_|) (re:string) (inp:string) =
-        if Regex(re).IsMatch(inp) then Some() else None
+        if Regex(re).IsMatch(inp) 
+        then Some() else None
 
-    let isAllCaps s = 
-        String.exists System.Char.IsLetter s 
-        && not (String.exists System.Char.IsLower s)
+    let (|IsAllCaps|_|) s = 
+        if String.exists System.Char.IsLetter s 
+            && not (String.exists System.Char.IsLower s) 
+        then Some() else None
 
     let hey words = 
         match words with
-        | words when isAllCaps words -> "Whoa, chill out!"
+        | IsAllCaps -> "Whoa, chill out!"
         | IsMatch "[?]$" -> "Sure."
         | IsMatch "^\s*$" -> "Fine. Be that way!"
         | _ -> "Whatever."
---
+++ src/Exercism/Triangle.fs
@@ -0,0 +1,11 @@
+﻿namespace Exercism
+
+module Triangle =
+
+    type TriangleKind =
+        | Equilateral
+        | Isosceles
+        | Scalene
+
+    let kind len1 len2 len3 =
+        TriangleKind.Equilateral
