cb26ed2e4228c5ea6bc8cd0eeca05540ead10c0a
nickc-ci <nchamberlain@cambridge-intl.com>
2016-06-21 22:17:07 -0400

    triangle

--- src/Exercism/Triangle.fs
+++ src/Exercism/Triangle.fs
@@ -7,5 +7,11 @@ module Triangle =
         | Isosceles
         | Scalene
 
-    let kind len1 len2 len3 =
-        TriangleKind.Equilateral
+    let kind (l1:decimal) (l2:decimal) (l3:decimal) =
+        if (l1 + l2 <= l3) || (l2 + l3 <= l1) || (l1 + l3 <= l2) || (l1 = 0m && l2 = 0m && l3 = 0m) then
+            raise (System.InvalidOperationException ("Illegal triangle"))
+        elif l1 = l2 && l2 = l3 && l1 = l3 then
+            TriangleKind.Equilateral
+        elif l1 = l2 || l1 = l3 || l2 = l3 then
+            TriangleKind.Isosceles
+        else TriangleKind.Scalene
