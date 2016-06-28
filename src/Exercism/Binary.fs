90e2de5197897ef436f70ff87b16b5efbec15038
nickc-ci <nchamberlain@cambridge-intl.com>
2016-06-28 17:08:29 -0400

    binary

---
+++ src/Exercism/Binary.fs
@@ -0,0 +1,11 @@
+﻿namespace Exercism
+
+module Binary = 
+    let parseCharToInt (c : char) = 
+        let (couldParse, parsedInt) = System.Int32.TryParse(System.Char.ToString(c))
+        if couldParse then Some(parsedInt)
+        else None
+    
+    let toDecimal (s : string) = 
+        //        if String.forall System.Char.IsNumber s then
+        Array.init s.Length (fun n -> parseCharToInt (s.Chars(n)))
