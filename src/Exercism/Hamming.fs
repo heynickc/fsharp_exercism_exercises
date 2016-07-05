6e093571587c0407a2315952b7819fc499e9825d
nickc-ci <nchamberlain@cambridge-intl.com>
2016-07-05 16:02:39 -0400

    hamming

--- src/Exercism/Hamming.fs
+++ src/Exercism/Hamming.fs
@@ -1,8 +1,25 @@
 ﻿namespace Exercism
 
 module Hamming = 
+    let compareChars charOne charTwo = 
+        match charOne = charTwo with
+        | true -> 0
+        | false -> 1
     
-    let compute (s1 : string) (s2 : string) =
-        Array2D.zeroCreate (3) (3)
-        0
-
+    let distance (strOne : string) (strTwo : string) len = 
+        len
+        |> Array.zeroCreate
+        |> Array.mapi (fun i _ -> compareChars (strOne.Chars(i)) (strTwo.Chars(i)))
+        |> Array.reduce (+)
+    
+    let isComputable strOne strTwo = 
+        let strOneLen = strOne |> String.length
+        let strTwoLen = strTwo |> String.length
+        match strOneLen > 0 && strTwoLen > 0 && strOneLen = strTwoLen with
+        | true -> Some(strOneLen)
+        | false -> None
+    
+    let compute strOne strTwo = 
+        match isComputable strOne strTwo with
+        | Some(x) -> distance strOne strTwo x
+        | None -> 0
