040fc8698c9700b7405e375717889ac40455193c
nickc-ci <nchamberlain@cambridge-intl.com>
2016-07-27 17:08:29 -0400

    piglatin

--- src/Exercism/PigLatin.fs
+++ src/Exercism/PigLatin.fs
@@ -1,7 +1,26 @@
 ﻿namespace Exercism
 
-module PigLatin =
+module PigLatin = 
+    let vowels = [| "a"; "e"; "i"; "o"; "u"; "yt"; "xr" |]
+    let extraConsonants = [| "ch"; "qu"; "th"; "thr"; "sch"; |]
+    type CharacterMatch = | CharacterMatch of string * int
+   
+    let (|StartsWithVowel|_|) (w : string) = 
+        if vowels |> Array.exists (fun x -> w.StartsWith(x)) then Some(w)
+        else None
+    
+    let (|StartsWithExtraConsonant|_|) (w: string)=
+        if extraConsonants |> Array.exists (fun x -> w.StartsWith(x)) then Some((w))
+        else None
 
-    let translate word = 
-        raise (new System.NotImplementedException())
-
+    let (|StartsWithConsonant|_|) (w : string) = 
+        match w with
+        | StartsWithVowel w -> None
+        | StartsWithExtraConsonant w -> Some(w) 
+        | _ -> Some(w)
+    
+    let translate (w : string) = 
+        match w with
+        | StartsWithVowel w -> w + "ay"
+        | StartsWithConsonant w -> w.Substring(1, w.Length - 1) + w.[0].ToString() + "ay"
+        | _ -> "nope"
--- tests/Exercism.Tests/PigLatinTests.fs
+++ tests/Exercism.Tests/PigLatinTests.fs
@@ -18,38 +18,29 @@ module PigLatinTests =
     let ``First letter and ay are moved to the end of words that start with consonants`` (word) = translate word
     
     [<Test>]
-    [<Ignore("Remove to run test")>]
     let ``Ch is treated like a single consonant``() = Assert.That(translate "chair", Is.EqualTo("airchay"))
     
     [<Test>]
-    [<Ignore("Remove to run test")>]
     let ``Qu is treated like a single consonant``() = Assert.That(translate "queen", Is.EqualTo("eenquay"))
     
     [<Test>]
-    [<Ignore("Remove to run test")>]
     let ``Qu and a single preceding consonant are treated like a single consonant``() = 
         Assert.That(translate "square", Is.EqualTo("aresquay"))
     
     [<Test>]
-    [<Ignore("Remove to run test")>]
     let ``Th is treated like a single consonant``() = Assert.That(translate "therapy", Is.EqualTo("erapythay"))
     
     [<Test>]
-    [<Ignore("Remove to run test")>]
     let ``Thr is treated like a single consonant``() = Assert.That(translate "thrush", Is.EqualTo("ushthray"))
     
     [<Test>]
-    [<Ignore("Remove to run test")>]
     let ``Sch is treated like a single consonant``() = Assert.That(translate "school", Is.EqualTo("oolschay"))
     
     [<Test>]
-    [<Ignore("Remove to run test")>]
     let ``Yt is treated like a single vowel``() = Assert.That(translate "yttria", Is.EqualTo("yttriaay"))
     
     [<Test>]
-    [<Ignore("Remove to run test")>]
     let ``Xr is treated like a single vowel``() = Assert.That(translate "xray", Is.EqualTo("xrayay"))
     
     [<Test>]
-    [<Ignore("Remove to run test")>]
     let ``Phrases are translated``() = Assert.That(translate "quick fast run", Is.EqualTo("ickquay astfay unray"))
